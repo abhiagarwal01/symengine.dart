3af548483f76e87bee5721ab94f8b858a05bdd83
Abhi Agarwal <abhiaagarwal01@gmail.com>
2022-09-21 08:41:53 -0400

    Add Package Template

---
+++ lib/symengine.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library symengine;
+
+export 'src/symengine_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
